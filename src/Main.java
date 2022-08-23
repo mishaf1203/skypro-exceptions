c55c25f86dd7eeef322f5eee67c83de97095a9a0
mishaf1203 <mishashaferoff@gmail.com>
2022-08-23 22:28:15 +0300

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -8,27 +8,28 @@ public class Main {
         String confirmPassword = "sadkz2478";
         System.out.println(checkCorrect(login, password,confirmPassword));
         String loginNegative = "dsfk 123";
-        String passwordNegative = "sadkz2343242342342478";
-        String confirmPasswordNegative = "sadkz2343242342342478";
+        String passwordNegative = "sadk34247877777777777777";
+        String confirmPasswordNegative = "sadk34247877777777777777";
         System.out.println(checkCorrect(loginNegative , passwordNegative,confirmPasswordNegative));
     }
 
     public static boolean checkCorrect(String login, String password, String confirmPassword) throws WrongPasswordException {
-        if (login.toCharArray().length > 20) {
-            throw new WrongLoginException("Слишком длинный логин");
+        try {
+
+            if (login.toCharArray().length > 20) {
+                throw new WrongLoginException("Слишком длинный логин");
+            }
+            if (password.toCharArray().length >= 20 || confirmPassword.toCharArray().length >= 20) {
+                throw new WrongLoginException("Слишком длинный пароль");
+            }
+            if (!password.equals(confirmPassword)) {
+                throw new WrongPasswordException("Пароли не совпадают");
+            }
+        } catch (WrongLoginException | WrongPasswordException exception){
+            System.out.println(exception.getMessage());
+            return false;
         }
-        if (password.toCharArray().length > 20 || confirmPassword.toCharArray().length >20){
-            throw new WrongLoginException("Слишком длинный пароль");
-        }
-        if (!login.matches("\\w{1,20}")) {
-            throw new WrongLoginException("Логин не соответствует условиям");
-        }
-        if (!password.matches("\\w{1,20}") || !confirmPassword.matches("\\w{1,20}"))  {
-            throw new WrongPasswordException("Пароль не соответствует условиям");
-        }
-        if (!password.equals(confirmPassword)) {
-            throw new WrongPasswordException("Пароли не совпадают");
-        }
+
         return true;
     }
 
