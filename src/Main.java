d8afd2e48ab38c90b9ec842362d0d81858842ab1
mishaf1203 <mishashaferoff@gmail.com>
2022-08-26 00:31:24 +0300

    3 commit

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ import exceptions.WrongPasswordException;
 
 public class Main {
     public static void main(String[] args) throws WrongPasswordException {
-        String login = "dsfk123";
+        String login = "dsf123";
         String password = "sadkz2478";
         String confirmPassword = "sadkz2478";
         System.out.println(checkCorrect(login, password,confirmPassword));
-        String loginNegative = "dsfk 123";
+        String loginNegative = "dsf123";
         String passwordNegative = "sadk34247877777777777777";
         String confirmPasswordNegative = "sadk34247877777777777777";
         System.out.println(checkCorrect(loginNegative , passwordNegative,confirmPasswordNegative));
@@ -16,11 +16,11 @@ public class Main {
     public static boolean checkCorrect(String login, String password, String confirmPassword) throws WrongPasswordException {
         try {
 
-            if (login.toCharArray().length > 20) {
-                throw new WrongLoginException("Слишком длинный логин");
+            if (!login.matches("\\w{1,20}")) {
+                throw new WrongLoginException("Логин не соответствует условиям");
             }
-            if (password.toCharArray().length >= 20 || confirmPassword.toCharArray().length >= 20) {
-                throw new WrongLoginException("Слишком длинный пароль");
+            if (!password.matches("\\w{1,20}")) {
+                throw new WrongPasswordException("Пароль не соответствует условиям");
             }
             if (!password.equals(confirmPassword)) {
                 throw new WrongPasswordException("Пароли не совпадают");
