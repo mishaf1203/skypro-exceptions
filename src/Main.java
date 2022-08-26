908f84c43e6615aca2e85ee38bac29eb2df4a4b4
mishaf1203 <mishashaferoff@gmail.com>
2022-08-26 12:21:54 +0300

    4 commit

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,19 @@ public class Main {
         String confirmPassword = "sadkz2478";
         System.out.println(checkCorrect(login, password,confirmPassword));
         String loginNegative = "dsf123";
-        String passwordNegative = "sadk34247877777777777777";
+        String passwordNegative = "1234578911234567891";
         String confirmPasswordNegative = "sadk34247877777777777777";
         System.out.println(checkCorrect(loginNegative , passwordNegative,confirmPasswordNegative));
     }
 
     public static boolean checkCorrect(String login, String password, String confirmPassword) throws WrongPasswordException {
         try {
-
+            if (login.toCharArray().length >= 20) {
+                throw new WrongLoginException("Слишком длинный логин");
+            }
+            if (password.toCharArray().length >= 20 || confirmPassword.toCharArray().length >=20) {
+                throw new WrongPasswordException("Слишком длинный пароль");
+            }
             if (!login.matches("\\w{1,20}")) {
                 throw new WrongLoginException("Логин не соответствует условиям");
             }
